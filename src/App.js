6444ee0e17aae09eb4e9e0b7392f1c907a1c2e26
TuzDmitry <dimatuz94@gmail.com>
2020-03-09 13:34:55 +0300

    fdfsdfsdfs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>I am Dima</h1>
+        <h1>I am Dimafdsgfdgdfgdd</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
