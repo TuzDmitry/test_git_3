6de12def17b2dc3a4c4a14069ec5d96538173d86
TuzDmitry <dimatuz94@gmail.com>
2020-04-05 01:40:31 +0300

    future test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          i will check clone my repo into new
         </p>
         <h1>I am Dima</h1>
         <a
